07fe371d2723e167691cbe51e40ef66df903ae0f
leammurphy <leam.murphy1@gmail.com>
2021-09-13 09:55:18 -0600

    initial set up of the views

--- adopt-pets/src/store/index.js
+++ adopt-pets/src/store/index.js
@@ -1,3 +1,5 @@
+// This is a new Vuex store which is a global object. Any data that is required in other parts of this application
+// is stored here and imported to the component
 import Vue from "vue";
 import Vuex from "vuex";
 
