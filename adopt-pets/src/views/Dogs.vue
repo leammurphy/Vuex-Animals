4f4c883efeca5d471129a2a3637e909f75c2218c
leammurphy <leam.murphy1@gmail.com>
2021-09-14 09:00:23 -0600

    Bootstrap properly imported. Created a simple dataset for cats and dogs which is now imported and used within the dogs and cats vue files.

--- adopt-pets/src/views/Dogs.vue
+++ adopt-pets/src/views/Dogs.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
-        Dogs
+        <b-table striped hover :items="dogs"></b-table>
     </div>
 </template>
+
+<script>
+import dogs from '@/data/dogs'
+
+export default {
+    data() {
+        return {
+            dogs
+        }
+    },
+}
+</script>
